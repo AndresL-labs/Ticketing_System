06247f459675a3fb8cb3a3af5c16c84ca282ff00
Andrsxfelipe <afelipelondono81@gmail.com>
2025-11-25 13:39:09 -0500

    Advanced in Hexagonal Arquitecture with Events Entity

--- src/main/java/com/example/ticketing_system/domain/port/in/ManageEventUseCase.java
+++ src/main/java/com/example/ticketing_system/domain/port/in/ManageEventUseCase.java
@@ -1,4 +1,13 @@
 package com.example.ticketing_system.domain.port.in;
 
-public class ManageEventUseCase {
+import com.example.ticketing_system.domain.model.Event;
+
+import java.util.List;
+
+public interface ManageEventUseCase {
+    Event getEventById(Long id);
+    List<Event> getAllEvents(int page, int size);
+    Event createEvent(Event newEvent);
+    Event updateEvent(Long id, Event event);
+    void deleteEvent(Long id);
 }
--- src/main/java/com/example/ticketing_system/domain/port/out/EventRepositoryPort.java
+++ src/main/java/com/example/ticketing_system/domain/port/out/EventRepositoryPort.java
@@ -1,4 +1,13 @@
 package com.example.ticketing_system.domain.port.out;
 
-public class EventRepositoryPort {
+import com.example.ticketing_system.domain.model.Event;
+
+import java.util.List;
+
+public interface EventRepositoryPort {
+    Event findById(Long id);
+    List<Event> findAll(int page, int size);
+    Event save(Event event);
+    Event update(Long id, Event event);
+    void delete(Long id);
 }
